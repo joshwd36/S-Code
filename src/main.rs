7a4c2006ca7be6d63e28ccde9fe60245e651e001
joshwd36 <joshwd36@gmail.com>
2020-06-22 22:49:13 +0100

    Fix duplicates

--- src/main.rs
+++ src/main.rs
@@ -13,28 +13,39 @@ fn main() {
         }
         let splits: Vec<_> = buffer.split(",").collect();
         if splits.len() == 3 {
-            if let (Ok(grade), Ok(credits), Ok(s_code)) = (splits[0].parse(), splits[1].parse(), splits[2].parse()) {
+            if let (Ok(grade), Ok(credits), Ok(s_code)) =
+                (splits[0].parse(), splits[1].parse(), splits[2].parse())
+            {
                 modules.push((grade, credits, s_code));
                 continue;
             }
         }
         println!("Incorrect line");
     }
+    modules.sort_unstable_by(|(a, _, _), (b, _, _)| a.partial_cmp(b).unwrap());
 
-
-    modules.sort_unstable_by(|(a, _, _), (b, _, _) | a.partial_cmp(b).unwrap());
-
-    let (s_codeable, base) : (Vec<_>, Vec<_>) = modules.iter().partition(|x| x.2);
+    let (s_codeable, base): (Vec<_>, Vec<_>) = modules.iter().partition(|x| x.2);
     for i in 0..=s_codeable.len() {
-        for combination in s_codeable.iter().combinations(i) {
+        for mut combination in s_codeable.iter().combinations(i) {
             let mut modules = base.clone();
-            let s_coded: Vec<_> = s_codeable.iter().filter(|x| !combination.contains(x)).collect();
-            modules.extend(combination);
+            modules.extend(combination.clone());
+            let s_coded: Vec<_> = s_codeable
+                .iter()
+                .filter(|x| remove_item(&mut combination, x).is_none())
+                .collect();
             if modules.len() == 0 {
                 break;
             }
 
-            let (total_grade, total_credits) = modules.iter().fold((0., 0), |(total_grade, total_credits), (grade, credits, _)| (total_grade + *grade * *credits as f32, total_credits + *credits));
+            let (total_grade, total_credits) = modules.iter().fold(
+                (0., 0),
+                |(total_grade, total_credits), (grade, credits, _)| {
+                    (
+                        total_grade + *grade * *credits as f32,
+                        total_credits + *credits,
+                    )
+                },
+            );
             let mean = total_grade / total_credits as f32;
             let mid = modules.len() / 2;
             let median = if modules.len() % 2 == 1 {
@@ -42,7 +53,15 @@ fn main() {
             } else {
                 (modules[mid - 1].0 + modules[mid].0) / 2.0
             };
-            println!("mean: {}, median: {}, total credits: {}, s-code: {:?}", mean, median, total_credits, s_coded);
+            println!(
+                "mean: {}, median: {}, total credits: {}, s-code: {:?}",
+                mean, median, total_credits, s_coded
+            );
         }
     }
+}
+
+fn remove_item<T: PartialEq>(vec: &mut Vec<T>, item: &T) -> Option<T> {
+    let index = vec.iter().position(|x| x == item)?;
+    Some(vec.remove(index))
 }
